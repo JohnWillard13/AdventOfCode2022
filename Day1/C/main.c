f15be14880c4f8f8a7c17ffddc0315b448cd8ab9
Ryan <ryanberrypi@protonmail.com>
2022-12-02 05:28:39 -0600

    Modified comments

--- Day1/C/main.c
+++ Day1/C/main.c
@@ -20,13 +20,16 @@ int main(int argc, char* argv []){
         int t_num = atoi(str);
         char c = getc(f);
 
+        //Adds current line's value to 'sum'
         sum = sum + t_num;
+
+        //Checks if the new sum is higher than hsum, if so, then it changes hsum to the new highest value
         if(sum > hSum){
             hSum = sum;
         }
 
+        //Checks if the line is a blank/empty line OR if EOF has been achieved - if so, resets sum to zero.
         if(strlen(str) < 3 || c == EOF){
-            //Compare sum to hSum then reset sum back to 0
             sum = 0;
         }
         ungetc(c,f);
@@ -34,6 +37,7 @@ int main(int argc, char* argv []){
 
     printf("\n");
     fclose(f);
+    //Prints the total calories the elf with the highest amount of calories is carrying
     printf("The elf that is carrying the highest amount of calories is carrying %d calories! Wow!\n",hSum);
 
 	return 0;
